e7123201a65ce85efc830c045ef729b016c0a7fb
ixiaolu2023 <148319690+ixiaolu2023@users.noreply.github.com>
2026-03-14 02:05:15 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 添加 Passwall 及其依赖的数据源
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# (可选) 添加其他常用插件源
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
