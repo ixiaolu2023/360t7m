d8fe0eab1897ba6d0b9e1d8ff4dcb148b72f19b0
ixiaolu2023 <148319690+ixiaolu2023@users.noreply.github.com>
2026-03-14 22:27:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,3 +3,5 @@
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # (可选) 添加其他常用插件源
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# 强制 Mediatek Filogic 系列使用 5.15 内核
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.15/g' target/linux/mediatek/Makefile
